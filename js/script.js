708cd30be4bbfbafdfe62aa4aff5196b80c65c17
Largo365 <97722764+Largo365@users.noreply.github.com>
2022-02-15 14:31:27 +0200

    Update script.js

--- js/script.js
+++ js/script.js
@@ -596,14 +596,87 @@ new Swiper('.before-vaccine__slide__wrapper',{
 	slideActiveClass: 'slide__active',
 });
 //-------------------------------------------------------------------------------------
-//-------------------------------СЛАЙД КОНТАКТНАЯ ИНФОРМАЦИЯ---------------------------
+//-------------------------------РАЗДЕЛ КОНТАКТНАЯ ИНФОРМАЦИЯ--------------------------
 //-------------------------------------------------------------------------------------
+//--Открытие слайда с информацией
 $(document).ready(function() {
 	$(".contact__title").click(function(event) {
-		if ($(this).parent('.contact-departament').find('.map_ballon').css('display') == 'block') {
-			$(this).parent('.contact-departament').find('.map_ballon').slideUp();
+		if ($(this).parent('.contact-departament').find('.map__content').css('display') == 'block') {
+			$(this).parent('.contact-departament').find('.map__content').slideUp();
+			$(this).parent('.contact-departament').find('.contact__title').find('span').css("transform","rotate(90deg)");
 		} else {
-			$(this).parent('.contact-departament').find('.map_ballon').slideDown();
+			$(this).parent('.contact-departament').find('.map__content').slideDown();
+			$(this).parent('.contact-departament').find('.contact__title').find('span').css("transform","rotate(45deg)");
 		};
 	});
+});
+//--Обновление адреса в модапе
+$(document).ready(function() {
+	$('.map__link').click(function() {
+		var adress = $(this).closest('.map__content').find('.map__adress').text();
+
+		$('.map-small.popup').find('.map-small__description').text(adress);
+		$('.map-small.popup').addClass('open');
+	});
+});
+
+
+
+//-------------------------------------------------------------------------------------
+//-------------------------------------КАРТА-------------------------------------------
+//-------------------------------------------------------------------------------------
+// Пример реализации боковой панели на основе наследования от collection.Item.
+// Боковая панель отображает информацию, которую мы ей передали.
+ymaps.modules.define('Panel', [
+    'util.augment',
+    'collection.Item'
+], function (provide, augment, item) {
+    // Создаем собственный класс.
+    var Panel = function (options) {
+        Panel.superclass.constructor.call(this, options);
+    };
+
+    // И наследуем его от collection.Item.
+    augment(Panel, item, {
+        onAddToMap: function (map) {
+            Panel.superclass.onAddToMap.call(this, map);
+            this.getParent().getChildElement(this).then(this._onGetChildElement, this);
+            // Добавим отступы на карту.
+            // Отступы могут учитываться при установке текущей видимой области карты,
+            // чтобы добиться наилучшего отображения данных на карте.
+            map.margin.addArea({
+                top: 0,
+                left: 0,
+                width: '250px',
+                height: '100%'
+            })
+        },
+
+        onRemoveFromMap: function (oldMap) {
+            if (this._$control) {
+                this._$control.remove();
+            }
+            Panel.superclass.onRemoveFromMap.call(this, oldMap);
+        },
+
+        _onGetChildElement: function (parentDomContainer) {
+            // Создаем HTML-элемент с текстом.
+            // По-умолчанию HTML-элемент скрыт.
+            this._$control = $('<div class="customControl"><div class="map_ballon"></div><div class="closeButton"></div></div>').appendTo(parentDomContainer);
+            this._$content = $('.map_ballon');
+            // При клике по крестику будем скрывать панель.
+            $('.closeButton').on('click', this._onClose);
+        },
+        _onClose: function () {
+            $('.customControl').css('display', 'none');
+        },
+        // Метод задания контента панели.
+        setContent: function (text) {
+            // При задании контента будем показывать панель.
+            this._$control.css('display', 'flex');
+            this._$content.html(text);
+        }
+    });
+
+    provide(Panel);
 });
